d8b1a9f6b308d37b941e017acd8be3417abc0d22
RENATA CARVALHO <2022140@student.cct.ie>
2022-11-15 23:43:00 +0000

    Altered background to BB colors (css)

--- src/app/pages/character-details/character-details.page.ts
+++ src/app/pages/character-details/character-details.page.ts
@@ -17,7 +17,7 @@ export class CharacterDetailsPage implements OnInit {
   ngOnInit() {
     this.characterId = this.activatedRoute.snapshot.paramMap.get('id');
     this.api.getCharacter(this.characterId).subscribe(res => {
-      this.character = res[0]; //It is necessary check inside the array
+      this.character = res[0]; 
     });
   }
 
